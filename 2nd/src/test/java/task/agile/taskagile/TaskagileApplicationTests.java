8eb80e5556c99a388042bd9e82b71b3c00612331
YoungChulShin <go1323@gmail.com>
2020-11-07 21:52:08 +0900

    scaffold 생성

---
+++ 2nd/src/test/java/task/agile/taskagile/TaskagileApplicationTests.java
@@ -0,0 +1,13 @@
+package task.agile.taskagile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskagileApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
